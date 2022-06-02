28754259c4697570669c94e2d9e7f9fae9fd2843
Michael Cassidy <100804225+Michael-Cassidy-88@users.noreply.github.com>
2022-06-02 12:27:06 -0500

    Create Git_Push.sh

---
+++ Git_Push.sh
@@ -0,0 +1,13 @@
+# Set up GitHub
+# Clone Repository
+# Provide GitHub Repo URL
+# Click on "main" and create new branch
+# To push code...
+git push --set-upstream origin <CURRENT_BRANCH>
+# Enter username and authentication token
+
+# To push to GitHub without credentials, set up git config...
+git config --global credential.helper store
+git push --set-upstream origin <CURRENT_BRANCH>
+# Enter username and authentication token
+# Now you will not need to enter credentials to pust code to GitHub Repo
